824cbd9fd8c442c49f527e00dd3e762cf25222e8
Nemesi Gergely Tibor <91251134+nemesigergelytibor-ILZGJC@users.noreply.github.com>
2021-09-23 11:19:53 +0200

    3_1 Feladat megoldása

--- 3_1_parossag_vizsgalat/main.c
+++ 3_1_parossag_vizsgalat/main.c
@@ -7,7 +7,25 @@ int main()
     printf("Parossag vizsgalata\n");
     printf("Kerem adjon meg egy egesz szamot\n");
     scanf("%d", &bekertEgeszSzam);
-    printf(bekertEgeszSzam ? "paros" : "paratlan");
+    printf(bekertEgeszSzam % 2 == 0 ? "(IF ROVID) paros" : "paratlan");
     printf("\n");
+    if(bekertEgeszSzam % 2 == 0)
+    {
+        printf("(IF HOSSZU) a bekert szam paros\n");
+    }
+    else
+    {
+        printf("(IF HOSSZU) a bekert szam paratlan\n");
+    }
+    switch (bekertEgeszSzam % 2)
+    {
+        case 0:
+            printf("(CASE 0) a bekert szam paros\n");
+            break;
+
+        case 1:
+            printf("(CASE 1) a bekert szam paratlan\n");
+            break;
+    }
     return 0;
 }
